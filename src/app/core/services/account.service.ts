af1fa7928906dec67a63b10cdb8b4f1767b21206
Rafael Andrade da Silva <rafael_andrade74@hotmail.com>
2026-08-23 11:06:27 -0300

    fix(painel): exibe nome do usuário cadastrado na API em vez do Keycloak
    AccountService agora mantém o último GetUserDto retornado (via
    obterUsuario/adicionarUsuario) em um BehaviorSubject, exposto como
    usuarioAtual/usuarioAtual$. O accountGuard já chama obterUsuario() ao
    entrar em /paineis, então esse cache é populado sem request extra.
    
    PainelList passa a montar o nome exibido a partir do firstName/lastName
    cadastrados, caindo para o nome do token do Keycloak apenas se o
    usuário da API ainda não tiver sido carregado.
    
    Refs #7

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { AddUserDto, GetUserDto } from '../models/usuario.model';
 
 @Injectable({ providedIn: 'root' })
 export class AccountService {
   private readonly baseUrl = `${environment.apiUrl}/account`;
+  private readonly usuarioAtualSubject = new BehaviorSubject<GetUserDto | undefined>(undefined);
+
+  readonly usuarioAtual$: Observable<GetUserDto | undefined> = this.usuarioAtualSubject.asObservable();
 
   constructor(private readonly http: HttpClient) {}
 
+  get usuarioAtual(): GetUserDto | undefined {
+    return this.usuarioAtualSubject.value;
+  }
+
   obterUsuario(): Observable<GetUserDto> {
-    return this.http.get<GetUserDto>(this.baseUrl);
+    return this.http.get<GetUserDto>(this.baseUrl).pipe(tap((usuario) => this.usuarioAtualSubject.next(usuario)));
   }
 
   adicionarUsuario(usuario: AddUserDto): Observable<GetUserDto> {
-    return this.http.post<GetUserDto>(this.baseUrl, usuario);
+    return this.http.post<GetUserDto>(this.baseUrl, usuario).pipe(tap((salvo) => this.usuarioAtualSubject.next(salvo)));
   }
 }
